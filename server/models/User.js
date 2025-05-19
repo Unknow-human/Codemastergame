923e8e76e0bccdfac1e57b846a3f1e94f7d8c671
Unknow-human <dtagboton@gmail.com>
2025-05-19 23:48:28 +0100

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,29 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, unique: true },
-  password: { type: String, required: true },
-  score: { type: Number, default: 0 },
-});
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  score: {
+    type: Number,
+    default: 0
+  }
+}, { timestamps: true });
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", userSchema);
