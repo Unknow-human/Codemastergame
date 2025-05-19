95c6db29031011ffb773aee622669b7e66b3be79
Unknow-human <dtagboton@gmail.com>
2025-05-19 21:29:12 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,9 @@
+const connectDB = require("./db");
+connectDB();
+
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("./models/User");
 const express = require("express");
 const path = require("path");
 const http = require("http");
