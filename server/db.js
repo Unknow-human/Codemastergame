e51ad5ba36991f762c2fe2f063fee82f4edf7bf3
Unknow-human <dtagboton@gmail.com>
2025-05-19 23:47:29 +0100

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useUnifiedTopology: true
     });
-    console.log("Connecté à MongoDB !");
+    console.log("MongoDB connecté avec succès");
   } catch (err) {
     console.error("Erreur de connexion MongoDB :", err.message);
     process.exit(1);
